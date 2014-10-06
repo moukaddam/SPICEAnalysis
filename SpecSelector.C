9db3b4d1e64928c90178fe72a256046b7083ceeb
moukaddam <mhd.moukaddam@gmail.com>
2014-10-06 14:15:08 -0700

    dynamic display of selected ring and sector fixed

--- SpecSelector.C
+++ SpecSelector.C
@@ -8,54 +8,88 @@
 using namespace std ; 
 
 // Root
-#include "TROOT.h"
-#include "TMath.h"
-#include "TFile.h"
-#include "TString.h"
-#include "TH1F.h"
+#include "TBenchmark.h"
 #include "TCanvas.h"
 #include "TColor.h"
+#include "TDirectory.h"
+#include "TEllipse.h"
 #include "TGraphErrors.h"
 #include "TGraph.h"
+#include "TH2Poly.h"
+#include "TH1F.h"
+#include "TFile.h"
+#include "TFolder.h"
+#include "TKey.h"
+#include "TLine.h"
+#include "TMath.h"
 #include "TMultiGraph.h"
 #include "TPolyLine.h"
-#include "TVector2.h"
-#include "TDirectory.h"
-#include "TH2Poly.h"
-#include "TKey.h"
-#include "TStyle.h"
-#include "TBenchmark.h"
 #include "TRandom3.h"
+#include "TROOT.h"
+#include "TString.h"
+#include "TStyle.h"
 #include "TVirtualX.h"
-
+#include "TVector2.h"
 
 
 // functions 
 void Stop();
 void SegmentClicked();
+void CreateSiLiMap();
+void OpenDataFile();
+int  OpenDataThisFile(TString filename);
+void GetHistogram(int indicator);
 #define STOP Stop();
 
 //Global variables 
-	TCanvas *gCanvas ;	
+TCanvas *gCanvas ;
+TCanvas *gCanvasHist ;	
+TFolder *gFolderHistos ; 
+
 
 //MAIN FUNCTION 
 void SpecSelector() {
-   
-	gCanvas = new TCanvas("SiLi_Segments","SiLi_Segments",10,10,700,700);	
-   gCanvas->ToggleEventStatus();
-   //gCanvas->SetGridx();
-   //gCanvas->SetGridy();
 
-   TFile *f;
-   f = TFile::Open("SiLiSegmentsGraph.root","READ");
+//Global settings
+   gStyle->SetOptStat(0000);
+   gStyle->SetPalette(1);   
 
-   TH2Poly *p = new TH2Poly("SiLi","SiLi (something in title...)",-20,+20,-20,+20);
-   //p->GetXaxis()->SetNdivisions(520);
-   p->GetXaxis()->SetTitle("X");
-   p->GetYaxis()->SetTitle("Y");
+//Open File for the histigram selection 
+	OpenDataFile();
+// Set the Mapping parameters, basically it will be an ascii file (in case the mapping is right during the experiment, this can be done through the mnemonics.	
+	//SetSiLiMap();
+	
+//Create the main map used by the user	
+	CreateSiLiMap();
 
-   p->SetContour(100);
+}
 
+
+void CreateSiLiMap(){
+
+
+	if(!gCanvas) {
+		//gCanvas->Delete();
+		TString title = "Bi207" ;
+		gCanvas = new TCanvas("SiLi_Segments","SiLi_Segments",10,10,700,700);
+		gCanvas->ToggleEventStatus();
+		//gCanvas->SetGridx();
+		//gCanvas->SetGridy();
+		gCanvas->AddExec("ex","SegmentClicked()");
+		//gCanvas->Divide(1,3) ; 
+		}
+	
+			
+//Create The h2 poly graph 
+   TH2Poly *H2Poly = new TH2Poly("SiLi","SiLi (something in title...)",-20,+20,-20,+20);
+   //H2Poly->GetXaxis()->SetNdivisions(520);
+   H2Poly->GetXaxis()->SetTitle("X");
+   H2Poly->GetYaxis()->SetTitle("Y");
+   H2Poly->SetContour(100);
+
+
+//Add the bin map 
+   TFile *f = TFile::Open("SiLiSegmentsGraph.root","READ");
    TMultiGraph *mg;
    TKey *key;
 	TObject* obj;
@@ -63,36 +97,30 @@ void SpecSelector() {
    while (key = (TKey*)nextkey()) {
       obj = key->ReadObj();
       if (obj->InheritsFrom("TGraph")) {
-         mg = (TMultiGraph*)obj;
+			mg = (TMultiGraph*)obj;
 			if (mg->GetName()=="all") continue ; 
-         p->AddBin(mg);
-      }
+			H2Poly->AddBin(mg);
+      	}
    }
 
 
-   gBenchmark->Start("Partitioning");
-   p->ChangePartition(100, 100);
-   gBenchmark->Show("Partitioning");
+   //gBenchmark->Start("Partitioning");
+   H2Poly->ChangePartition(100, 100);
+   //gBenchmark->Show("Partitioning");
 
+//Fill the map (I nthe future fill it withthe number f content from a specefic range of energy, useful if it's calibrated')
    TRandom3 r;
-   gBenchmark->Start("Filling");
-   for (int i=0; i<5000; i++) {
+   //gBenchmark->Start("Filling");
+   for (int i=0; i<10000; i++) {
       float x = r.Uniform(-15,+15);
       float y  = r.Uniform(-15,+15);
-      p->Fill(x,y);
+      H2Poly->Fill(x,y);
    }
-   gBenchmark->Show("Filling");
+  //gBenchmark->Show("Filling");
 
-   gStyle->SetOptStat(0000);
-   gStyle->SetPalette(1);
-   p->Draw("COL");
-
-gCanvas->AddExec("ex","SegmentClicked()");
-			
+   H2Poly->Draw("COL");	
+	
 }
-
-
- 
 
 void SegmentClicked() {
 	//this action function is called whenever you move the mouse
@@ -138,66 +166,167 @@ void SegmentClicked() {
 		int pxmax = gPad->XtoAbsPixel(uxmax);
 		//cout << "pxmin, pxmax  " << pxmin << " " << pxmax << endl;
 
-		if(pyold) gVirtualX->DrawLine(pxmin,pyold,pxmax,pyold);
-		gVirtualX->DrawLine(pxmin,y,pxmax,y);
-		gPad->SetUniqueID(py);
+		//if(pyold) gVirtualX->DrawLine(pxmin,pyold,pxmax,pyold);
+		//gVirtualX->DrawLine(pxmin,y,pxmax,y);
+		//gPad->SetUniqueID(py);
 		//Float_t upy = gPad->AbsPixeltoY(py);
 		//Float_t y = gPad->PadtoY(upy);
 
-//TLine *line = new TLine(0,0,0,c2->GetUymax());
-//line->Draw();
-
-//create or set the new canvas c2
-		TVirtualPad *padsav = gPad;
-		TCanvas *c2 = (TCanvas*)gROOT->GetListOfCanvases()->FindObject("c2");
-		if(c2) delete c2->GetPrimitive("Projection"); // Delete the (primitive) subpad of the object named "Projection" 
-		else   c2 = new TCanvas("c2","Projection Canvas",710,10,400,400);
-		c2->SetGrid();
-		c2->cd();
-
-//Draw Histogram corresponding to mouse positionon the new Canvas	
+//detecotr parameters
+		float InnerRadius = 5 ; 
+		float OuterRadius = 15 ; 
+		int   NumberOfRings = 10 ; 		
+		//TLine *line = new TLine(0,0,x,y);
+		//line->Draw();
 		
-		//  find bin 
-		//  cout<< "bin #" <<h2poly->FindBin(x,y) <<endl ;
-		//  Int_t biny = h->GetYaxis()->FindBin(y);
+		float theta = TMath::ATan(y/x) * TMath::RadToDeg(); // in Radians 
+		if (x < 0 ) theta = theta + 180 ;  
+		float angle1 = theta-(15);
+		float angle2 = angle1+(30);
 
-		//call the right histogram for now its dummy
-		TH1F *hp = new TH1F("Projection","hist title",100,-20,20);
-		TRandom ran;
-		ran.SetSeed(x*y) ;
-		for (int i = 0; i<1000; i++) {
-		hp->Fill(ran.Gaus(0,3));
-		}
+		TEllipse  *lsector = new TEllipse(0,0,OuterRadius,0,angle1,angle2);
+		lsector->SetFillStyle(0);
+		lsector->SetLineWidth(2);
+   		lsector->Draw();
+   		 
+   		 float RingInnerRadius = TMath::Sqrt(x*x+y*y) - 0.5*(OuterRadius-InnerRadius)/NumberOfRings; 
+   		 float RingOuterRadius = RingInnerRadius + (OuterRadius-InnerRadius)/NumberOfRings;  
+		 TEllipse  *lring1 = new TEllipse(0.0,0.0,RingInnerRadius,RingInnerRadius);
+		 TEllipse  *lring2 = new TEllipse(0.0,0.0,RingOuterRadius,RingOuterRadius);
+		 lring1->SetFillStyle(0);
+		 lring1->SetLineColor(1);
+		 lring2->SetLineWidth(2);
+   		 lring2->SetFillStyle(lring1->GetFillStyle());
+   		 lring2->SetLineColor(lring1->GetLineColor());
+   		 lring1->SetLineWidth(lring2->GetLineWidth());
+   		 lring1->Draw();
+   		 lring2->Draw();
+  
 
-		// Modify the histogram
-		hp->SetFillColor(38);
-		hp->Draw();
-
-		//update canvas	
-		//gCanvas->Update();					
-		c2->Update();
-		padsav->cd();
-
+		gCanvas->Update();
+		//line->Delete();
+		lring1->Delete();
+		lring2->Delete();
+		lsector->Delete();
 		//return ;
 	}
 
-/*
+
 		if (event == 12) {
 		cout<< "Middle click" <<endl ;
 
 		//TH2Poly *h2poly = (TH2Poly*)select;
 		cout<< "x and y :" <<x << "\t"<< y <<endl ;
 		cout<< "bin #" <<h2poly->FindBin(x,y) <<endl ;
-      int binxy = h2poly->FindBin(x,y) ;
+        int binxy = h2poly->FindBin(x,y) ;
 		cout<< "bin content" <<h2poly->GetBinContent( binxy ) <<endl ;
+		
+		//Draw Histogram corresponding to mouse positionon the new Canvas	
+		
+		GetHistogram( binxy ) ; 
+
+		//update canvas	
+		//gCanvas->Update();					
+		gCanvasHist->Update();
+		//padsav->cd();
 		}
-*/
+
 
 		return ;
 
 }
 
 
+//_______________________________________
+ void OpenDataFile() {
+ 
+ // add command asking for the name/path...
+ TString fname = "his30093"; 
+ 
+ OpenDataThisFile(fname) ; 
+ }
+ 
+//_______________________________________ 
+ int OpenDataThisFile(TString fname ) {
+ 
+ 	int success = 0 ;
+ 	
+ 	//TString fname = "his30093" ; 
+       // open the ROOT file to process
+
+   TString path ="/data1/moukaddam/SpiceTestSep2014/Calibration/Files/";
+   TString extension =  ".root" ;
+   TString inFileName = fname+extension;
+   TFile *inFile = new TFile(path + inFileName);
+   
+   if ( ! inFile->IsOpen() )  { //try present directory
+   path ="./";
+   inFile->SetName(path + inFileName);
+   }
+   else {
+   success = 1 ;
+   cout << "file is opened "<< endl; 
+    inFile->ls();   	
+   } 
+
+	   if (gDebug) { 
+	   cout << "Opening the root file and grabing histograms " << inFile->GetName() << endl ;  
+	   inFile->ls();
+	   getchar();
+	   }
+	   
+	 gFolderHistos = (TFolder*)(inFile->FindObjectAny("histos"));
+	 return success ;
+   }  
+   
+  
+  //___________________________
+void GetHistogram(int channel  ) {
+
+//make sure file is opened 
+	if (!gFolderHistos) {
+		cout << " You need to open a histogram folder first " <<endl ; 
+		return ; 
+	}
+	
+
+	//create or set the new canvas gCanvasHist
+		//TVirtualPad *padsav = gPad;
+		gCanvasHist = (TCanvas*)gROOT->GetListOfCanvases()->FindObject("gCanvasHist");
+		if(gCanvasHist) delete gCanvasHist->GetPrimitive("Projection"); // Delete the (primitive) subpad of the object named "Projection" 
+		else   gCanvasHist = new TCanvas("gCanvasHist","Projection Canvas",710,10,700,700);
+		//gCanvasHist->SetGrid();
+		gCanvasHist->SetFrameFillColor(kBlack);
+		gCanvasHist->SetCrosshair(2);
+		gCanvasHist->cd();
+		
+       
+	TString hname = Form("Chrg%d", channel+1060); 
+	TH1F* hist = (TH1F*)(gFolderHistos->FindObjectAny(hname));
+	cout << "=========================================";
+	cout << " Retreiving Channel : " << channel ;
+	cout << " ========================================="<< endl;
+
+	//gCanvas->cd(1);
+	hist->GetXaxis()->SetRangeUser(0,3500);
+	hist->GetXaxis()->SetTitle("Energy (keV)");
+	hist->GetXaxis()->SetAxisColor(kRed);
+	hist->GetXaxis()->SetTickLength(-0.01);
+	
+	hist->GetYaxis()->SetTitle("Counts ");
+	hist->GetYaxis()->SetAxisColor(kRed);
+	hist->GetYaxis()->SetTickLength(-0.01);
+	
+	hist->SetLineColor(kYellow);
+	
+	//Draw on the other Canvas
+	gCanvasHist->cd() ;
+	hist->Draw(); 
+	
+	gCanvasHist->ToggleEventStatus();
+	gCanvasHist->Update(); 
+}
+ 
 
 void Stop() {
 	char c; 
